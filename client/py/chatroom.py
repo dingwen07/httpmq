9565fee927e5da65ecfd5467c190945b3a5bbc33
Dingwen Wang <wangdingwen32@gmail.com>
2023-07-22 17:08:46 +0800

    Update chatroom.py

--- client/py/chatroom.py
+++ client/py/chatroom.py
@@ -1,25 +1,54 @@
 import time
 import platform
 import threading
+import queue
 import curses
 import curses.textpad
 from httpmqchatroom import HTTPMQChatroom, ChatroomMessage
+
+HELP_MESSAGE = '''\
+
+Commands:
+    /nickname <nickname>    Change nickname
+    /leave                  Leave the chatroom
+    /clear                  Clear the screen
+    /help                   Show this help message
+'''
 
 def input_with_default(prompt, default):
     result = input(f'{prompt} (Default is {default}): ')
     return result if result else default
 
-def receive_func(pad, client, max_y, max_x, stdscr):
+def receive_func(pad, client, max_y, max_x, stdscr, screen_control_queue):
+    time.sleep(0.3)
     row = 0
+    screen_content: list[str] = []
+    next_delay = 0
     while True:
         try:
+            y, x = curses.getsyx()
             refresh = False
             # chechk if the terminal size has changed
             new_max_y, new_max_x = stdscr.getmaxyx()
             if new_max_x != max_x or new_max_y != max_y:
-                pad.resize(1000, new_max_x)
+                pad.clear()
+                row = 0
+                for line in screen_content:
+                    pad.addstr(row, 0, line)
+                    row += int((len(line) - 1) / max_x) + 1
                 refresh = True
-            max_x, max_y = new_max_x, new_max_y
+                max_x, max_y = new_max_x, new_max_y
+                next_delay = -1
+            if not screen_control_queue.empty():
+                control = screen_control_queue.get()
+                if control == 0:
+                    pad.clear()
+                    pad.noutrefresh(0, 0, 0, 0, max_y-3, max_x-1)
+                    row = 0
+                else:
+                    row += control
+                curses.setsyx(y, x)
+                refresh = True
             messages = client.receive()
             messages.sort(key=lambda x: x['timestamp'])
             for chat_message in messages:
@@ -28,43 +57,64 @@ def receive_func(pad, client, max_y, max_x, stdscr):
                     timestamp = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(chat_message["timestamp"]))
                     message = f'[{timestamp}] {message_text}'
                     pad.addstr(row, 0, message)
-                    row += 1
+                    screen_content.append(message)
+                    row += int((len(message) - 1) / max_x) + 1
                     refresh = True
             if refresh:
                 min_row = 0 if row < max_y else row - max_y + 2
                 pad.noutrefresh(min_row, 0, 0, 0, max_y-3, max_x-1)
+                curses.setsyx(y, x)
             curses.doupdate()
         except:
             pass
         finally:
-            time.sleep(0.5)
+            if next_delay < 0:
+                pass
+            if next_delay > 0:
+                time.sleep(next_delay)
+            else:
+                time.sleep(0.3)
+            next_delay = 0
 
 def chat(stdscr, client):
     # stdscr.nodelay(True)
     max_y, max_x = stdscr.getmaxyx()
     pad = curses.newpad(1000, max_x)  # create a new pad to store history of messages
     pad.scrollok(True)  # allow the pad to scroll
+    screen_control_queue = queue.Queue()
 
-    threading.Thread(target=receive_func, args=(pad, client, max_y, max_x, stdscr), daemon=True).start()
+    threading.Thread(target=receive_func, args=(pad, client, max_y, max_x, stdscr, screen_control_queue), daemon=True).start()
 
     input_prompt = 'Input: '
-    print(input_prompt, end='', flush=True)
+    leave_prompt = '/Press Enter now or Hit Ctrl-C again to Leave'
+    leave_commands = ['/leave', '/quit', '/exit', leave_prompt]
     input_message = ''  # initialize variable to store user's input message
-    try:
-        while True:
+    stdscr.addstr(max_y - 1, 0, f'Input: {input_message}')
+    curses.setsyx(max_y - 1, len(input_prompt))
+    while True:
+        try:
+            ch = None
             try:
                 ch = stdscr.get_wch()
             except curses.error:
-                continue
+                stdscr.nodelay(False)
             if isinstance(ch, str):
                 if ch == '\n':
                     if input_message.startswith('/nickname '):
                         new_nickname = input_message[10:]
                         client.update_nickname(new_nickname)
-                    elif input_message == '/leave':
-                        client.send_leave()
-                        break
-                    else:
+                    elif input_message in leave_commands:
+                        raise KeyboardInterrupt
+                    elif input_message == '/clear':
+                        screen_control_queue.put(0)
+                    elif input_message == '/help':
+                        pad.addstr(HELP_MESSAGE)
+                        pad.noutrefresh(0, 0, 0, 0, max_y-3, max_x-1)
+                        curses.setsyx(max_y - 1, len(input_prompt))
+                        screen_control_queue.put(HELP_MESSAGE.count('\n'))
+                    elif input_message.startswith('/'):
+                        continue
+                    elif input_message != '':
                         client.send_message(input_message)
                     input_message = ''
                 elif ch == '\b' or ch == '\x7f':
@@ -87,14 +137,20 @@ def chat(stdscr, client):
             stdscr.refresh()
 
             # time.sleep(0.01)
-    except KeyboardInterrupt:
-        client.send_leave()
-    finally:
-        curses.nocbreak()
-        stdscr.keypad(False)
-        curses.echo()
-        curses.endwin()
-        print('Bye')
+        except KeyboardInterrupt:
+            if input_message in leave_commands:
+                client.send_leave()
+                curses.nocbreak()
+                stdscr.keypad(False)
+                curses.echo()
+                curses.endwin()
+                print('Bye')
+                exit(0)
+            else:
+                input_message = leave_prompt
+                stdscr.nodelay(True)
+        except Exception as e:
+            print(e)
 
 if __name__ == '__main__':
     try:
